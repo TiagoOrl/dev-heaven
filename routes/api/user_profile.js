b20723081ff1fb5867ce179a572f7102df1a351c
Tiago <tiago@pop-os.localdomain>
2021-07-04 19:11:59 -0300

    implemented API routes for adding/removing experiences and education in UserProfile model

--- routes/api/user_profile.js
+++ routes/api/user_profile.js
@@ -232,4 +232,106 @@ async (req, res) => {
     }
 });
 
+
+//@route    PUT api/profile/education
+// @desc    Add user profile education
+// @access  Private
+router.put('/education', [authenticator, [
+    check('school', 'school name is required').not().isEmpty(),
+    check('degree', 'degree type is required').not().isEmpty(),
+    check('fieldOfStudy', 'fieldOfStudy is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty()
+]], 
+async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty)
+        return res.status(400).json({ errors: errors.array() });
+
+    const {
+        school,
+        degree,
+        fieldOfStudy,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const educationItem = {
+        school,
+        degree,
+        fieldOfStudy,
+        from, 
+        to,
+        current,
+        description
+    };
+    
+    try {
+        
+        // req.user.id comes from the auth token
+        const userProfile = await UserProfile.findOne({ user: req.user.id  });
+
+        if (!userProfile) 
+            return res.status(400).json({ msg: 'There is no profile for this user' });
+
+        userProfile.education.unshift(educationItem);
+        await userProfile.save();
+
+        return res.json(userProfile);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Internal error');
+    }
+});
+
+
+//@route    DELETE api/profile/experience/:exp_id
+// @desc    Removes a single experience item from the UserProfile experiences list
+// @access  Private
+router.delete('/experience/:exp_id', authenticator, 
+async (req, res) => {
+
+    try {
+        const userProfile = await UserProfile.findOne({ user: req.user.id });
+
+        // Get remove index        
+        const removeIndex = userProfile.experience.map(item => item.id).indexOf(req.params.exp_id);
+        userProfile.experience.splice(removeIndex, 1);
+        await userProfile.save();
+
+        return res.json(userProfile);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Internal error');
+    }
+
+});
+
+//@route    DELETE api/profile/experience/:edu_id
+// @desc    Removes a single education item from the UserProfile educations list
+// @access  Private
+router.delete('/education/:edu_id', authenticator, 
+async (req, res) => {
+
+    try {
+        const userProfile = await UserProfile.findOne({ user: req.user.id });
+
+        // Get remove index        
+        const removeIndex = userProfile.education.map(item => item.id).indexOf(req.params.edu_id);
+        userProfile.education.splice(removeIndex, 1);
+        await userProfile.save();
+
+        return res.json(userProfile);
+
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).send('Internal error');
+    }
+
+});
+
 module.exports = router;
