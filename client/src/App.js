4623f532d4a483870d9f8a3f342d93229afd97f7
Tiago <tiago@pop-os.localdomain>
2021-07-07 19:21:37 -0300

    Created Register React Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,28 @@
+import Login from './components/login_register/Login';
+import Register from './components/login_register/Register';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import React,  {Fragment} from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar/>
-      <Landing/>
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar/>
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register}/>
+            <Route exact path="/login" component={Login}/>
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
 
   );
 }
 
 export default App;
+
---
+++ client/src/components/login_register/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            Login User
+        </div>
+    )
+}
+
+
+export default Login;
